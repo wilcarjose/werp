33db4b2d012bc6c7d5f524a8f32f6c420dd10f5c
wilcar <wilcarjose@gmail.com>
2019-08-21 13:58:25 -0400

    Updated products import

--- app/Modules/Core/Products/Imports/ProductsImport.php
+++ app/Modules/Core/Products/Imports/ProductsImport.php
@@ -3,7 +3,10 @@
 namespace Werp\Modules\Core\Products\Imports;
 
 use Maatwebsite\Excel\Concerns\ToModel;
+use Werp\Modules\Core\Products\Models\Uom;
+use Werp\Modules\Core\Products\Models\Brand;
 use Werp\Modules\Core\Products\Models\Product;
+use Werp\Modules\Core\Products\Models\Category;
 
 class ProductsImport implements ToModel
 {
@@ -15,9 +18,51 @@ class ProductsImport implements ToModel
     public function model(array $row)
     {
         return new Product([
-           'code'     => $row[0],
-           'name'    => $row[1], 
-           'description' => $row[2],
+            'code'     => $row[0],
+            'name'    => $row[1], 
+            'description' => $row[2],
+            'part_number' => $row[3],
+            'barcode' => $row[7],
+            'link' => $row[8],
+            'brand_id' => $this->getBrandId($row[5]),
+            'category_id' => $this->getCategoryId($row[6]),
+            'uom_id' => $this->getUomId($row[4]),
         ]);
     }
+
+    protected function getBrandId($name)
+    {
+        if (empty($name)) {
+            return null;
+        }
+
+        $brand = Brand::firstOrCreate(['name' => trim($name)]);
+
+        return $brand->id;
+    } 
+
+    protected function getCategoryId($name)
+    {
+        if (empty($name)) {
+            return null;
+        }
+
+        $brand = Category::firstOrCreate([
+          'name' => trim($name),
+          'type' => Category::PRODUCT_TYPE
+        ]);
+
+        return $brand->id;
+    } 
+
+    protected function getUomId($name)
+    {
+        if (empty($name)) {
+            return null;
+        }
+
+        $brand = Uom::firstOrCreate(['name' => trim($name)]);
+
+        return $brand->id;
+    } 
 }
